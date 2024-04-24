50d67e98a0100c6073ca90e21ccddf73f48d84a1
Aurore-Be <aurore.berthet@hotmail.com>
2024-04-24 17:18:14 +0200

    Update Bubbles Dialogue script and prefab, now working

--- Assets/Dialogues/TestsBubbles/TalkingBuddy.cs
+++ Assets/Dialogues/TestsBubbles/TalkingBuddy.cs
@@ -7,37 +7,92 @@ using Yarn.Unity;
 
 public class TalkingBuddy : MonoBehaviour
 {
-    [SerializeField] GameObject Canvas;
-    [SerializeField] DialogueRunner dialogueRunner;
+    [SerializeField] GameObject dialogueViewPrefab;
 
-    [Header("Dialogues")]
+    [Header("Dialogues Infos")]
     [SerializeField] List<string> linesNames;
 
-    public float timeBeforeTalking = 3;
-    public float actualTime = 0;
-    public string nextLineToSay = "";
+    [Header("Settings Timer Before Talking")]
+    public int TimeBeforeTalking = 5;
+
+    public bool RandomizeTimeBT = false;
+    /// <summary>
+    /// BT is for Before Talking
+    /// /!\ inclus
+    /// </summary>
+    public int minimalTimeBT = 3;
+    /// <summary>
+    /// BT is for Before Talking
+    /// /!\ exclu
+    /// </summary>
+    public int maximalTimeBT = 7;
+
+    #region Private Attributes
+    private string nextLineToSay = "";
+    private bool isTalking = false;
+    private Transform dialoguePosition;
+    private GameObject actualDialogueObject = null;
+    private float actualTime = 0;
+    DialogueRunner dialogueRunner;   
+    private float timeBeforeTalking = 3;
+    #endregion
 
     private void Start()
     {
-        dialogueRunner.onDialogueComplete.AddListener(InitLine);
-        InitLine();
+        dialoguePosition = this.transform;
+        RandomizeLine();
+        ResetTimer();
+        timeBeforeTalking = Random.Range(minimalTimeBT, maximalTimeBT);
     }
 
     private void Update()
     {
         actualTime += Time.deltaTime;
-        if (actualTime > timeBeforeTalking)
+        if (isTalking)
         {
-            dialogueRunner.StartDialogue(nextLineToSay);
-            actualTime = 0.0f;
+            if (actualTime > timeBeforeTalking)
+            {
+                StopDialogue();
+                ResetTimer();
+            }
+        }
+        else
+        {
+            if (actualTime > timeBeforeTalking)
+            {
+                InitDialogue();
+                ResetTimer();
+            }
         }
     }
-    private void InitLine()
+
+    #region Private Methods
+    private void ResetTimer()
+    {
+        actualTime = 0.0f;
+    }
+    private void RandomizeLine()
     {
         nextLineToSay = linesNames[Random.Range(0, linesNames.Count)];
-        actualTime = 0.0f;
-        timeBeforeTalking = Random.Range(3, 7);
     }
+    private void InitDialogue()
+    {
+        // Spawn view & setup dialogueRunner
+        actualDialogueObject = Instantiate(dialogueViewPrefab, dialoguePosition);
+        actualDialogueObject.transform.parent = this.transform;
+        dialogueRunner = actualDialogueObject.GetComponent<DialogueRunner>();
+
+        // Init Dialogue
+        dialogueRunner.StartDialogue(nextLineToSay);
+        isTalking = true;
+    }
+
+    private void StopDialogue()
+    {
+        Destroy(actualDialogueObject);
+        timeBeforeTalking = Random.Range(minimalTimeBT, maximalTimeBT);
+        RandomizeLine();
+        isTalking = false;
+    }
+    #endregion
 }
-
-
