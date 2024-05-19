c8f746e947d2efda948e6448fc7f13dce0bd680a
Under_Scroll <tifabval@gmail.com>
2024-05-19 14:27:50 +0200

    Fix simulation

--- Assets/Scripts/Simulation/Simulator.cs
+++ Assets/Scripts/Simulation/Simulator.cs
@@ -20,21 +20,21 @@ namespace Simulation
 
             _structure = structure;
 
-            instantiateBlocks();
-            createConnections();
+            InstantiateBlocks();
+            CreateConnections();
         }
 
-        private void instantiateBlocks()
+        private void InstantiateBlocks()
         {
             foreach (CellData cellData in _structure.Cells)
             {
                 if (cellData == null) continue;
 
-                instanciateBlock(cellData);
+                InstanciateBlock(cellData);
             }
         }
 
-        private GameObject instanciateBlock(CellData cellData)
+        private GameObject InstanciateBlock(CellData cellData)
         {
             GameObject instance = Instantiate(cellData.Type.Block, _structureOrigin);
 
@@ -49,7 +49,7 @@ namespace Simulation
             return instance;
         }
 
-        private void createConnections()
+        private void CreateConnections()
         {
             foreach (CellData cellData in _structure.Cells)
             {
@@ -59,46 +59,55 @@ namespace Simulation
 
                 //Add north connection
                 ConnectionType connectionType = cellData.GetConnectionType(CellData.Face.North);
-                int3 northBlockPosition = new int3(cellData.position.x, cellData.position.y, cellData.position.z + 1);
+                int3 northBlockPosition = new(cellData.position.x, cellData.position.y, cellData.position.z + 1);
                 if (connectionType != null 
-                    && isInBounds(northBlockPosition)
-                    && cellData.Type.hasConnection(CellData.Face.North)
-                    && _structure.Cells[northBlockPosition.x, northBlockPosition.y, northBlockPosition.z].Type.hasConnection(CellData.Face.South))
+                    && IsInBounds(northBlockPosition)
+                    && cellData.Type.hasConnection(CellData.Face.North))
                 {
-                    GameObject northBlock = _instances[northBlockPosition.x, northBlockPosition.y, northBlockPosition.z];
-                    if (northBlock != null)
-                        addConnection(blockInstance, northBlock, connectionType);
+                    if (_structure.Cells[northBlockPosition.x, northBlockPosition.y, northBlockPosition.z] != null
+                        && _structure.Cells[northBlockPosition.x, northBlockPosition.y, northBlockPosition.z].Type.hasConnection(CellData.Face.South))
+                    {
+                        GameObject northBlock = _instances[northBlockPosition.x, northBlockPosition.y, northBlockPosition.z];
+                        if (northBlock != null)
+                            AddConnection(blockInstance, northBlock, connectionType);
+                    }
                 }
 
                 //Add east connection
                 connectionType = cellData.GetConnectionType(CellData.Face.East);
-                int3 eastBlockPosition = new int3(cellData.position.x + 1, cellData.position.y, cellData.position.z);
+                int3 eastBlockPosition = new(cellData.position.x + 1, cellData.position.y, cellData.position.z);
                 if (connectionType != null
-                    && isInBounds(eastBlockPosition)
-                    && cellData.Type.hasConnection(CellData.Face.East)
-                    && _structure.Cells[eastBlockPosition.x, eastBlockPosition.y, eastBlockPosition.z].Type.hasConnection(CellData.Face.Bottom))
+                    && IsInBounds(eastBlockPosition)
+                    && cellData.Type.hasConnection(CellData.Face.North))
                 {
-                    GameObject eastBlock = _instances[eastBlockPosition.x, eastBlockPosition.y, eastBlockPosition.z];
-                    if (eastBlock!= null)
-                        addConnection(blockInstance, eastBlock, connectionType);
+                    if (_structure.Cells[eastBlockPosition.x, eastBlockPosition.y, eastBlockPosition.z] != null
+                        && _structure.Cells[eastBlockPosition.x, eastBlockPosition.y, eastBlockPosition.z].Type.hasConnection(CellData.Face.South))
+                    {
+                        GameObject eastBlock = _instances[eastBlockPosition.x, eastBlockPosition.y, eastBlockPosition.z];
+                        if (eastBlock != null)
+                            AddConnection(blockInstance, eastBlock, connectionType);
+                    }
                 }
 
                 //Add top connection
                 connectionType = cellData.GetConnectionType(CellData.Face.Top);
-                int3 topBlockPosition = new int3(cellData.position.x, cellData.position.y + 1, cellData.position.z);
-                if (connectionType != null 
-                    && isInBounds(topBlockPosition)
-                    && cellData.Type.hasConnection(CellData.Face.Top)
-                    && _structure.Cells[topBlockPosition.x, topBlockPosition.y, topBlockPosition.z].Type.hasConnection(CellData.Face.Bottom))
+                int3 topBlockPosition = new(cellData.position.x, cellData.position.y + 1, cellData.position.z);
+                if (connectionType != null
+                    && IsInBounds(topBlockPosition)
+                    && cellData.Type.hasConnection(CellData.Face.North))
                 {
-                    GameObject topBlock = _instances[topBlockPosition.x, topBlockPosition.y, topBlockPosition.z];
-                    if (topBlock != null)
-                        addConnection(blockInstance, topBlock, connectionType);
+                    if (_structure.Cells[topBlockPosition.x, topBlockPosition.y, topBlockPosition.z] != null
+                        && _structure.Cells[topBlockPosition.x, topBlockPosition.y, topBlockPosition.z].Type.hasConnection(CellData.Face.South))
+                    {
+                        GameObject topBlock = _instances[topBlockPosition.x, topBlockPosition.y, topBlockPosition.z];
+                        if (topBlock != null)
+                            AddConnection(blockInstance, topBlock, connectionType);
+                    }
                 }
             }
         }
 
-        private bool isInBounds(int3 position)
+        private bool IsInBounds(int3 position)
         {
             return position.x >= 0
                 && position.y >= 0
@@ -108,7 +117,7 @@ namespace Simulation
                 && position.z < _structure.Cells.GetLength(2);
         }
 
-        private void addConnection(GameObject obj, GameObject to, ConnectionType type)
+        private void AddConnection(GameObject obj, GameObject to, ConnectionType type)
         {
             FixedJoint joint = obj.AddComponent<FixedJoint>();
             joint.breakForce = type.BreakForce;
