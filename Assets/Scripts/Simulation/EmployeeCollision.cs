6a9e7a4c7ffca9255444c42ec080be91ba6954b5
Mattéo Blivet <blivet.pro@gmail.com>
2024-06-05 15:39:23 +0200

    Fix transition issues on music,
    +add employee collision sound, concrete collision sound with magnitude

--- Assets/Scripts/Simulation/EmployeeCollision.cs
+++ Assets/Scripts/Simulation/EmployeeCollision.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
@@ -7,10 +8,27 @@ using UnityEngine.EventSystems;
 public class EmployeeCollision : MonoBehaviour
 {
     public static UnityEvent collisionEvent = new();
+    private bool IsFirstGroundCollision = true;
+    
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Ground")) 
+        if (collision.gameObject.CompareTag("Ground"))
+        {
             collisionEvent.Invoke();
+
+            float collisionForce = collision.impulse.magnitude;
+            if (IsFirstGroundCollision)
+            {
+                AkSoundEngine.SetRTPCValue("Collision_Velocity", collisionForce);
+                IsFirstGroundCollision = false;
+            }
+            if (collisionForce > 3)
+            {
+                
+                AkSoundEngine.PostEvent("Play_Physics_employeeCollision", gameObject);
+                Debug.Log(collisionForce);
+            }
+        }
     }
 }
