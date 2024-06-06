d8121766622315705945cab204d6c00a8f4691fb
Aurore-Be <124353787+Aurore-Be@users.noreply.github.com>
2024-06-06 16:59:24 +0200

    update buttons on off visuals

--- Assets/Scripts/UI/UI_ONOFFButton.cs
+++ Assets/Scripts/UI/UI_ONOFFButton.cs
@@ -3,18 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using TMPro;
 
 public class UI_ONOFFButton : MonoBehaviour
 {
+    [Header("Buttons")]
     [SerializeField] Button on;
+    [SerializeField] TextMeshProUGUI textOn;
     [SerializeField] Button off;
+    [SerializeField] TextMeshProUGUI textOff;
+
     [SerializeField] GameObject slider;
+
+    [SerializeField] Color _OnColor;
+    [SerializeField] Color _OffColor;
+
 
     public void Move(bool _on)
     {
-        if(_on)
+        if (_on)
+        {
             slider.transform.DOMoveX(on.transform.position.x, 0.5f);
+            ChangeColors(_on);
+        }
         else
+        {
             slider.transform.DOMoveX(off.transform.position.x, 0.5f);
+            ChangeColors(_on);
+        }
+    }
+    private void ChangeColors(bool _on)
+    {
+        if (_on)
+        {
+            textOn.color = _OnColor;
+            textOff.color = _OffColor;
+        }
+        else
+        {
+            textOn.color = _OffColor;
+            textOff.color= _OnColor;
+        }
     }
 }
