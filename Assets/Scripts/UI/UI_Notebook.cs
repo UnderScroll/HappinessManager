168c7f56ec0fb90962b5d957541d349c63e74169
UnderScroll <tifabval@gmail.com>
2024-06-19 17:10:13 +0200

    Fix translation

--- Assets/Scripts/UI/UI_Notebook.cs
+++ Assets/Scripts/UI/UI_Notebook.cs
@@ -25,49 +25,49 @@ public class UI_Notebook : MonoBehaviour
         {
             case GameManager.Stage.Stage1:
                 stageName.text = "FLOOR 1 : BOSS WORKSPACE";
-                stageDescription.text = "\"Bienvenue !\r" +
-                    "\nJ’aime commencer fort avec mes nouveaux collaborateurs, c’est comme un rite de passage." +
-                    "Ma Comptable et moi-même avons besoin de travailler côte à côte, je veux que tu construises une structure qui relie nos bureaux. " +
-                    "À toi de jouer !\"\t\t\t";
+                stageDescription.text = "\"« Welcome!!\r" +
+                    "\nI like to hit the ground running with my new team members; it's akin to an initiation into the corporate culture" +
+                    "My accountant and I need to work side by side, so I want you to build a structure to link our desks. »" +
+                    "Time for you to shine!\"\t\t\t";
                 if (infoPanels[0] != null)
                     infoPanels[0].SetActive(true);
                 break;
             case GameManager.Stage.Stage2:
                 stageName.text = "FLOOR 2 : CAFETARIA";
-                stageDescription.text = "Et voilà notre cafétéria ! Comme tu peux le voir, c’est un endroit très apprécié par nos employés. " +
-                    "Cet espace informel a été pensé pour favoriser leur productivité : du café à volonté, pas de chaises pour éviter qu’ils ne s’installent et surtout, une corbeille de fruit frais toutes les semaines. " +
-                    "Car un collaborateur heureux est un collaborateur efficace ! \r\nÇa me tient énormément à cœur tu sais qu’ils puissent se relaxer ensemble, ça améliore grandement l’aspect collaboratif. " +
-                    "Bref, assez discuté ! Je t’invite à regarder ton prochain projet : on a une petite problématique de fuite… Comme tu peux le voir, il y a de la mousse partout et ça devient dangereux ! " +
-                    "Je te laisse trouver une solution, ça serait dommage de fermer la cafétéria pour si peu.";
+                stageDescription.text = "« And here's our cafeteria! As you can see, it's a highly valued space by our employees. " +
+                    "This informal area has been designed to enhance their productivity: unlimited coffee, no chairs to prevent them from settling in, and most importantly, a fresh fruit basket every week. " +
+                    "Because a happy employee is an efficient one! It means a lot to me that they can relax together; it greatly enhances collaboration. " +
+                    "Anyway, enough talking! I invite you to look into your next project: we have a small leakage issue... As you can see, there's foam everywhere and it's becoming hazardous! " +
+                    "I'll let you find a solution; it would be a shame to close the cafeteria for such a trivial matter. »";
                 if (infoPanels[1] != null)
                     infoPanels[1].SetActive(true);
                 break;
             case GameManager.Stage.Stage3:
                 stageName.text = "FLOOR 3 : CO-WORKING FLOOR";
-                stageDescription.text = "« Quel zèle dans la cafétéria ! Ça se sent que tu es fait pour WorkBuddies, tes solutions sont aussi disruptives que créatives ! Laisse moi te présenter notre espace de coworking : " +
-                    "c’est ici qu’on teste toutes nos dernières trouvailles en terme d’organisation des employés et gestion de leur productivité. " +
-                    "Comme tu peux le voir, on mesure aujourd’hui la régulation et la gouvernance des dynamiques intra-démographiques au sein d’un même bureau !" +
-                    " …On dirait que ce n’est pas très parlant pour toi ? Ne t’en préoccupe pas, tu n’es qu’un Happiness Manager après tout ! " +
-                    "Anyway, au travail ! J’ai une RH en arrêts travail répétés à virer et une remplaçante à lui trouver. Et toi, un espace à designer ! »";
+                stageDescription.text = "« What a dynamo in the cafeteria! It's clear that you're made for WorkBuddies; your solutions are as disruptive as they are creative! Let me introduce you to our coworking space: " +
+                    "this is where we evaluate all our latest innovations in employee organization and productivity management. " +
+                    "As you can see, today we are measuring the regulation and governance of intra-demographic dynamics within a single office! " +
+                    "...It looks like that's not very clear to you? Don't worry about it, you're just a Happiness Manager after all! " +
+                    "Anyway, let's get to work! I have an HR person on repeated sick leave to fire and a replacement to find for her. And you have a space to design! »";
                 if (infoPanels[2] != null)
                     infoPanels[2].SetActive(true);
                 break;
             case GameManager.Stage.Stage4:
                 stageName.text = "FLOOR 4 : TEAMBUILDING SPACE";
-                stageDescription.text = "« Une équipe, c’est avant tout des gens soudés et capables de communiquer ! On ne peut malheureusement pas en dire autant de tout le monde… " +
-                    "Mais que veux-tu, les absents ont toujours tort, it’s their loss comme on dit ! L’entreprise peut tourner sans eux… " +
-                    "Des comptables et des employés, ça se recrute en un claquement de doigt ! Chez WorkBuddies on a besoin d’employés fidèles qui fit notre culture d’entreprise, pas de paresseux. " +
-                    "Assez discutaillé, c’est une mission de crise que je te confie aujourd’hui. Avec les employés restants, j’aimerais que tu mettes au goût des pratiques actuelles notre espace de team building ! " +
-                    "Avec ces pertes récentes, c’est important qu’ils se sentent valorisés et irremplaçables. »";
+                stageDescription.text = "« A team, it's primarily about people who are united and capable of communicating! Unfortunately, we can't say the same for everyone... " +
+                    "But hey, as they say, the absents are always wrong, it’s their loss! The company can function without them... " +
+                    "Accountants and employees, they can be recruited in the blink of an eye! At WorkBuddies, we need loyal employees who embody our company culture, not lazy ones. " +
+                    "Enough chatter, today I'm entrusting you with a crisis mission. With the remaining employees, I would like you to modernize our team building space to align with current practices! " +
+                    "Given these recent losses, it's important that they feel valued and indispensable. »";
                 if (infoPanels[3] != null)
                     infoPanels[3].SetActive(true);
                 break;
             case GameManager.Stage.Stage5:
                 stageName.text = "FLOOR 5 : INTERNS OFFICE";
-                stageDescription.text = "C’est la cata ! Entre les arrêts maladies à rallonge, les simples démissions et les soit disant « burn out », cette grande maladie imaginaire du 21ème siècle ! Cette entreprise est en train de couler ! " +
-                    "C’est comme ça qu’on me remercie ! « Pousse-les à être meilleurs » qu’on m’a dit, « tu verras tu seras récompensé » Tu parles ! Travailler ça n’intéresse plus personne, la satisfaction de l’effort est mort et cette entreprise avec ! " +
-                    "Je suis terriblement déçu, c’est une trahison sans nom ! Dans ces conditions tu comprendras que je ne peux pas continuer ta période d’essai, nous allons devoir nous séparer ici… " +
-                    "Hop hop hop, pas si vite ! Avant que tu fasses tes affaires, j’ai une dernière tâche à te confier. On a plus vraiment de budget mais je compte sur toi pour refaire à neuf le cagib- je veux dire le bureau des stagiaires !";
+                stageDescription.text = "« It’s a total mess! With the endless sick leaves, the simple resignations, and these so-called ‘burnouts’—the trendy imaginary disease of the 21st century! This company is sinking! " +
+                    "And this is how they show their appreciation? ‘Push them to excel,’ they said ‘you’ll be rewarded.’ Yeah, right! Nobody cares about hard work anymore; the gratification of effort has become obsolete, and so has this company! " +
+                    "I’m terribly disappointed: it’s a betrayal beyond words. Given the circumstances, you’ll understand that I can’t extend your probationary period; we’ll have to part ways here… " +
+                    "Hold on a second! Before you pack your things, I have one last task for you. We don't really have a budget anymore, but I'm counting on you to refurbish the storage room— I mean the intern's office! »";
                 if (infoPanels[4] != null)
                     infoPanels[4].SetActive(true);
                 break;
